6f5acc8fbbd8ad13143b8fa82a32b4a147839fe2
dangkhoa-fakemonika <vdkhoa22@clc.fitus.edu.vn>
2024-07-03 10:38:34 +0700

    sample login/register page
    first version of login/register

---
+++ src/lib/screen/register.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:lookout_dev/template/text_input_decorations.dart';
+
+class Register extends StatefulWidget {
+  final Function toggle;
+  const Register({super.key, required this.toggle});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+  // final AuthService _authService = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+
+
+  String email = '';
+  String password = '';
+  String errorMsg = 'none';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 0.0,
+          title: const Text('Log In'),
+          actions: <Widget>[
+            TextButton.icon(
+              icon: const Icon(Icons.person),
+              onPressed: () {},
+              label: const Text('Register'),
+            )
+          ],
+        ),
+        body: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 20.0,),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: "Email"),
+                  validator: (val) => val!.isEmpty ? 'Enter an email' : null,
+                  onChanged: (val){
+                    setState(() => email = val);
+                  },
+                ),
+                const SizedBox(height: 20.0,),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: "Password"),
+                  validator: (val) => val!.length < 6 ? 'Password must be longer than 6' : null,
+                  obscureText: true,
+                  onChanged: (val){
+                    setState(() => password = val);
+                  },
+                ),
+                ElevatedButton(
+                    onPressed: () async {},
+                    child: const Text("Sign in")
+                ),
+                const SizedBox(height: 20.0,),
+                Text(
+                  errorMsg,
+                  style: const TextStyle(color: Colors.red),
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+
+}
