bfcb8579a239f005cb4f69b937734ef45b843183
dangkhoa-fakemonika <vdkhoa22@clc.fitus.edu.vn>
2024-07-03 11:22:10 +0700

    additional build for previous commit
    just added a few updates + main authentication state widget

---
+++ src/lib/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:lookout_dev/screen/home.dart';
+import 'package:lookout_dev/screen/login.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return Placeholder();
+    // return Home();
+  }
+}
