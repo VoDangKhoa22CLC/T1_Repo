d17d5305c467f77ccaefc281f75d65333c7790cf
HaGiaBao22CLC <hgbao22@clc.fitus.edu.vn>
2024-08-28 15:30:41 +0700

    add validation for empty field in create event action, add showing picked datetime, fix screen overflow

--- src/lib/screen/info_screen/event_create.dart
+++ src/lib/screen/info_screen/event_create.dart
@@ -19,7 +19,7 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
   final _formKey = GlobalKey<FormState>();
   String _eventName = '';
   String _eventDescription = '';
-  DateTime _eventDate = DateTime(2024);
+  DateTime _eventDate = DateTime.now();
   String _eventLocation = '';
   String _eventNotes = '';
   final EventController eventController = EventController();
@@ -89,138 +89,170 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Event Name'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an event name';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _eventName = value!;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Event Description'),
-                onSaved: (value) {
-                  _eventDescription = value!;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Event Location'),
-                onSaved: (value) {
-                  _eventLocation = value!;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Event Notes'),
-                onSaved: (value) {
-                  _eventNotes = value!;
-                },
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Text(
-                    _eventDate == null
-                        ? 'Select Date'
-                        : 'Date: ${_eventDate!.toLocal()}'.split(' ')[0],
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.calendar_today),
-                    onPressed: () => _selectDate(context),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  _pickedImage[0] != null ? Container(
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Image.file(File(_pickedImage[0]!.path!), width: 120, height: 120,),
-                        IconButton(
-                          onPressed: (){_unselectPicture(0);},
-                          icon: const Icon(Icons.motion_photos_off_outlined),
-                          color: Colors.white,
-                        ),
-                      ]
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                // Enter event name
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Event Name'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter an event name';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _eventName = value!;
+                  },
+                ),
+                // Enter event description
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Event Description'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter an event description';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _eventDescription = value!;
+                  },
+                ),
+                // Enter event location
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Event Location'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter event location';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _eventLocation = value!;
+                  },
+                ),
+                // Enter event notes
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Event Notes'),
+                  onSaved: (value) {
+                    _eventNotes = value!;
+                  },
+                ),
+                // Select Date
+                TextField(
+                  focusNode: FocusNode(canRequestFocus: false),
+                  decoration: InputDecoration(
+                    labelText: _eventDate == null
+                          ? 'Select Date'
+                          : 'Date: ' + _eventDate!.toLocal().toString().split(' ')[0],
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.calendar_today),
+                      onPressed: () => _selectDate(context),
                     ),
-                  ) :
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      SizedBox(width: 120, height: 120),
-                      IconButton(
-                        onPressed: (){_selectPicture(0);},
-                        icon: const Icon(Icons.add_photo_alternate_outlined),
-                      )
-                    ],
-                  ),
-                  _pickedImage[1] != null ? Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Image.file(File(_pickedImage[1]!.path!), width: 120, height: 120,),
-                        IconButton(
-                          onPressed: (){_unselectPicture(1);},
-                          icon: const Icon(Icons.motion_photos_off_outlined),
-                          color: Colors.white,
-                        ),
-                      ]
-                  ) :
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      SizedBox(width: 120, height: 120),
-                      IconButton(
-                        onPressed: (){_selectPicture(1);},
-                        icon: const Icon(Icons.add_photo_alternate_outlined),
-                      )
-                    ],
-                  ),
-                  _pickedImage[2] != null ? Container(
-                    child: Stack(
+                  )
+                ),
+                // SizedBox(height: 20),
+                // Row(
+                //   children: [
+                //     Text(
+                //       _eventDate == null
+                //           ? 'Select Date'
+                //           : 'Date: ${_eventDate!.toLocal()}'.split(' ')[0],
+                //     ),
+                //     IconButton(
+                //       icon: Icon(Icons.calendar_today),
+                //       onPressed: () => _selectDate(context),
+                //     ),
+                //   ],
+                // ),
+                // const SizedBox(height: 20),
+                // Add image section
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    _pickedImage[0] != null ? Container(
+                      child: Stack(
                         alignment: Alignment.center,
                         children: [
-                          Image.file(File(_pickedImage[2]!.path!), width: 120, height: 120,),
+                          Image.file(File(_pickedImage[0]!.path!), width: 120, height: 120,),
                           IconButton(
-                            onPressed: (){_unselectPicture(2);},
+                            onPressed: (){_unselectPicture(0);},
                             icon: const Icon(Icons.motion_photos_off_outlined),
                             color: Colors.white,
                           ),
                         ]
+                      ),
+                    ) :
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(width: 120, height: 120),
+                        IconButton(
+                          onPressed: (){_selectPicture(0);},
+                          icon: const Icon(Icons.add_photo_alternate_outlined),
+                        )
+                      ],
                     ),
-                  ) :
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      SizedBox(width: 120, height: 120),
-                      IconButton(
-                        onPressed: (){_selectPicture(2);},
-                        icon: const Icon(Icons.add_photo_alternate_outlined),
-                      )
-                    ],
+                    _pickedImage[1] != null ? Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Image.file(File(_pickedImage[1]!.path!), width: 120, height: 120,),
+                          IconButton(
+                            onPressed: (){_unselectPicture(1);},
+                            icon: const Icon(Icons.motion_photos_off_outlined),
+                            color: Colors.white,
+                          ),
+                        ]
+                    ) :
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(width: 120, height: 120),
+                        IconButton(
+                          onPressed: (){_selectPicture(1);},
+                          icon: const Icon(Icons.add_photo_alternate_outlined),
+                        )
+                      ],
+                    ),
+                    _pickedImage[2] != null ? Container(
+                      child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Image.file(File(_pickedImage[2]!.path!), width: 120, height: 120,),
+                            IconButton(
+                              onPressed: (){_unselectPicture(2);},
+                              icon: const Icon(Icons.motion_photos_off_outlined),
+                              color: Colors.white,
+                            ),
+                          ]
+                      ),
+                    ) :
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(width: 120, height: 120),
+                        IconButton(
+                          onPressed: (){_selectPicture(2);},
+                          icon: const Icon(Icons.add_photo_alternate_outlined),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _createEvent,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor,
                   ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _createEvent,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Theme.of(context).primaryColor,
+                  child: const Text(
+                    'Create Event', 
+                    style: TextStyle(color: Colors.white), 
+                  ),
                 ),
-                child: const Text(
-                  'Create Event', 
-                  style: TextStyle(color: Colors.white), 
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
