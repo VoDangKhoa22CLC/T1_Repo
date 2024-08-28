516b01e9e47242651fce0f01ce6d988e07296506
Le Tri Man <120546650+KlAYq@users.noreply.github.com>
2024-08-28 23:25:38 +0700

    decorating EventTile class

--- src/lib/template/event_tile.dart
+++ src/lib/template/event_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lookout_dev/data/account_class.dart';
 import 'package:lookout_dev/data/event_class.dart';
-import 'package:lookout_dev/screen/info_screen/event_screen.dart';
+import 'package:lookout_dev/screen/info_screen/event_screen.dart';  
 // import 'package:test_backend/models/school_events.dart';
 
 class EventTile extends StatelessWidget {
@@ -8,30 +9,56 @@ class EventTile extends StatelessWidget {
   final EventClass myEvent;
 
   const EventTile({super.key, required this.myEvent});
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0),
-        child: Card(
-          margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EventScreen(myEvent: myEvent,))
-              );
-            },
-            child: ListTile(
-              leading: const Icon(
-                Icons.access_time_rounded,
-                color: Colors.black,
+      child: Card(
+        margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => EventScreen(myEvent: myEvent),
               ),
-              title: Text(myEvent.eventName),
-              subtitle: Text(myEvent.eventShortDescription),
+            );
+          },
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage('images/avatar_default.png'),
             ),
+            title: Text(
+              myEvent.eventName,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  myEvent.hostID,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  myEvent.eventTime,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-      )
+      ),
     );
   }
 }
